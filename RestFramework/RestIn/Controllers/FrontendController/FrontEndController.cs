af964dafdc7eebd529fba035b1062f647302888b
Ramachandran Anantharaman <ranantharaman@mtn.navis.com>
2022-08-21 15:47:42 +0530

    Returning hardcoded values

--- RestFramework/RestIn/Controllers/FrontendController/FrontEndController.cs
+++ RestFramework/RestIn/Controllers/FrontendController/FrontEndController.cs
@@ -25,26 +25,37 @@ namespace RestIn.Controllers
 
         [Route("getOccupancyValues")]
         [HttpGet]
-        public IActionResult GetOccupancyValues()
+        public JsonResult GetOccupancyValues()
         {
             DBConnection db = new DBConnection();
-           // string j = "values:{22:10,23:12,24:12}";
+            // string j = "values:{22:10,23:12,24:12}";
             //string j = "[{\"userId\": 1,\"id\": 1,\"title\": \"delectus aut autem\",\"completed\": false},{\"userId\": 1,\"id\": 2,\"title\": \"quisutnam\",\"completed\": false}]";
-           // string json = JsonConvert.SerializeObject(j);
+            // string json = JsonConvert.SerializeObject(j);
 
             //return Ok(json);
 
 
-            var jsonString = "{\"values\":[1,12,24]}";
-        //    Response.ContentType = new MediaTypeHeaderValue("application/json").ToString();
-          //  Response.WriteAsync(jsonString, Encoding.UTF8);
+            //var jsonString = "{\"actualValues":[2,24,36],\"predictedValues\":[2,24,36,42,54]}";
+            //var jsonString = "{"actualValues":[2,24,36],'predictedValues':[2,24,36,42,54]}";
+            int[] ac = { 2, 24, 36, 42, 54 };
+            int[] pr = { 2, 24, 36 };
+            var jsonString = new { actualValues=ac,predictedValues=pr};
+            //return Json(users, JsonRequestBehavior.AllowGet);
+
+            //    Response.ContentType = new MediaTypeHeaderValue("application/json").ToString();
+            //  Response.WriteAsync(jsonString, Encoding.UTF8);
 
             //return CreatedAtRoute("Result", json);
             // return Ok(json);
 
-            //var data = new { Name = "kevin", Age = 40 };
+            var data = new { Name = "kevin", Age = 40 };
             //return Json(jsonString);
-            return Ok(jsonString);
+            //return Ok(jsonString);
+            return this.Json(jsonString);
+            //return this.private object actualValues;
+
+        //Json(data);
+
         }
     }
 
